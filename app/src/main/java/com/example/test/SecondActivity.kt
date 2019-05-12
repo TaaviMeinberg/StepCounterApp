1ca7cada6659de04193e64b4aa5009f04abe020f
Elinor Roosalu <elinorroosalu@Aljos-MBP.lan>
2019-05-12 20:33:21 +0300

    No update

--- app/src/main/java/com/example/test/SecondActivity.kt
+++ app/src/main/java/com/example/test/SecondActivity.kt
@@ -2,6 +2,7 @@ package com.example.test
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class SecondActivity : AppCompatActivity() {
 
